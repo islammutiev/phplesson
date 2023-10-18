198e9c8fd6a7d2fe435eeedc3a1ccd0a02a56a86
islammutiev <140596653+islammutiev@users.noreply.github.com>
2023-10-18 15:46:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$firstNum = 42;
+$secondNum = 1337;
+
+$sum = $firstNum + $secondNum;
+$difference = $firstNum - $secondNum;
+$product = $firstNum * $secondNum;
+$quotient = $firstNum / $secondNum;
+
+$name = "Islam";
+$age = 17;
+
+echo "Сумма: $sum" . "<br>" . "Разность: $difference" . "<br>" . "Произведение: $product" . "<br>" . "Частное: $quotient" . "<br>" . "Hello, $name, your age is $age.";
+
+?>
